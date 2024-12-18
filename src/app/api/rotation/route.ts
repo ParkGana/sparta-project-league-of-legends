a7ea156199dcf896a8fc0a89e32a215945aba44e
ParkGana <gnchoco97@gmail.com>
2024-12-18 21:08:02 +0900

    feat : error 핸들링

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -3,34 +3,50 @@ import { RotationType } from '@/types/Rotation';
 import { NextResponse } from 'next/server';
 
 export async function GET(): Promise<NextResponse> {
-    // rotation 데이터
-    const rotationRes = await fetch('https://kr.api.riotgames.com/lol/platform/v3/champion-rotations', {
-        headers: {
-            'X-Riot-Token': `${process.env.NEXT_PUBLIC_RIOT_API_KEY}`
+    try {
+        // rotation 데이터
+        const rotationRes = await fetch('https://kr.api.riotgames.com/lol/platform/v3/champion-rotations', {
+            headers: {
+                'X-Riot-Token': `${process.env.NEXT_PUBLIC_RIOT_API_KEY}`
+            }
+        });
+
+        if (!rotationRes.ok) {
+            return NextResponse.json({ error: 'Failed to fetch rotation data' }, { status: rotationRes.status });
         }
-    });
 
-    const rotationData: RotationType = await rotationRes.json();
+        const rotationData: RotationType = await rotationRes.json();
 
-    // version 데이터
-    const versionRes = await fetch('https://ddragon.leagueoflegends.com/api/versions.json');
+        // version 데이터
+        const versionRes = await fetch('https://ddragon.leagueoflegends.com/api/versions.json');
 
-    const versionData: string[] = await versionRes.json();
+        if (!versionRes.ok) {
+            return NextResponse.json({ error: 'Failed to fetch version data' }, { status: versionRes.status });
+        }
 
-    // champions 데이터
-    const championsRes = await fetch(
-        `https://ddragon.leagueoflegends.com/cdn/${versionData[0]}/data/ko_KR/champion.json`
-    );
+        const versionData: string[] = await versionRes.json();
 
-    const data = await championsRes.json();
-    const championsData: ChampionsType = data.data;
+        // champions 데이터
+        const championsRes = await fetch(
+            `https://ddragon.leagueoflegends.com/cdn/${versionData[0]}/data/ko_KR/champion.json`
+        );
 
-    // rotation 데이터와 champions 데이터를 활용해서 최종 데이터 추출
-    const filteredData: ChampionsType = Object.fromEntries(
-        Object.entries(championsData).filter(([key, value]) =>
-            rotationData.freeChampionIds.includes(parseInt(value.key))
-        )
-    );
+        if (!championsRes.ok) {
+            return NextResponse.json({ error: 'Failed to fetch champions data' }, { status: championsRes.status });
+        }
 
-    return NextResponse.json(filteredData);
+        const data = await championsRes.json();
+        const championsData: ChampionsType = data.data;
+
+        // rotation 데이터와 champions 데이터를 활용해서 최종 데이터 추출
+        const filteredData: ChampionsType = Object.fromEntries(
+            Object.entries(championsData).filter(([key, value]) =>
+                rotationData?.freeChampionIds?.includes(parseInt(value.key))
+            )
+        );
+
+        return NextResponse.json(filteredData);
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: error.status });
+    }
 }
